e6c4815e15ffcd00ef5f60644bf853bb9315b56e
Марк Сидиэй <aclo.create@gmail.com>
2021-07-26 15:38:57 +0300

    Added translations

---
+++ l10n/asw_ru.ts
@@ -0,0 +1,380 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>AMenuBar</name>
+    <message>
+        <location filename="../widgets/a_menubar.cpp" line="27"/>
+        <source>File</source>
+        <translation>Файл</translation>
+    </message>
+    <message>
+        <location filename="../widgets/a_menubar.cpp" line="28"/>
+        <source>Source manager</source>
+        <translation>Менеджер источников</translation>
+    </message>
+    <message>
+        <location filename="../widgets/a_menubar.cpp" line="29"/>
+        <source>Export message history</source>
+        <translation>Экспортировать историю сообщений</translation>
+    </message>
+    <message>
+        <location filename="../widgets/a_menubar.cpp" line="30"/>
+        <source>Import message history</source>
+        <translation>Импортировать историю сообщений</translation>
+    </message>
+    <message>
+        <location filename="../widgets/a_menubar.cpp" line="31"/>
+        <source>Enable monologue mode</source>
+        <translation>Включить режим монолога</translation>
+    </message>
+    <message>
+        <location filename="../widgets/a_menubar.cpp" line="47"/>
+        <source>&amp;Exit</source>
+        <translation>&amp;Выход</translation>
+    </message>
+    <message>
+        <location filename="../widgets/a_menubar.cpp" line="53"/>
+        <source>Edit</source>
+        <translation>Правка</translation>
+    </message>
+    <message>
+        <location filename="../widgets/a_menubar.cpp" line="54"/>
+        <source>Clear message history</source>
+        <translation>Очистить историю сообщений</translation>
+    </message>
+    <message>
+        <location filename="../widgets/a_menubar.cpp" line="55"/>
+        <source>Delete</source>
+        <translation>Удалить</translation>
+    </message>
+    <message>
+        <location filename="../widgets/a_menubar.cpp" line="56"/>
+        <source>Cut</source>
+        <translation>Вырезать</translation>
+    </message>
+    <message>
+        <location filename="../widgets/a_menubar.cpp" line="57"/>
+        <source>Copy</source>
+        <translation>Копировать</translation>
+    </message>
+    <message>
+        <location filename="../widgets/a_menubar.cpp" line="58"/>
+        <source>Paste</source>
+        <translation>Вставить</translation>
+    </message>
+    <message>
+        <location filename="../widgets/a_menubar.cpp" line="59"/>
+        <source>Select all</source>
+        <translation>Выбрать всё</translation>
+    </message>
+    <message>
+        <location filename="../widgets/a_menubar.cpp" line="87"/>
+        <source>Tools</source>
+        <translation>Инструменты</translation>
+    </message>
+    <message>
+        <location filename="../widgets/a_menubar.cpp" line="88"/>
+        <source>Hide menubar</source>
+        <translation>Скрыть меню</translation>
+    </message>
+    <message>
+        <location filename="../widgets/a_menubar.cpp" line="89"/>
+        <source>Full screen</source>
+        <translation>Полноэкранный режим</translation>
+    </message>
+    <message>
+        <location filename="../widgets/a_menubar.cpp" line="91"/>
+        <source>Settings...</source>
+        <translation>Параметры...</translation>
+    </message>
+    <message>
+        <location filename="../widgets/a_menubar.cpp" line="105"/>
+        <source>Help</source>
+        <translation>Справка</translation>
+    </message>
+    <message>
+        <location filename="../widgets/a_menubar.cpp" line="106"/>
+        <source>About</source>
+        <translation>О приложении</translation>
+    </message>
+    <message>
+        <location filename="../widgets/a_menubar.cpp" line="109"/>
+        <source>About Qt</source>
+        <translation>О Qt</translation>
+    </message>
+</context>
+<context>
+    <name>ASW</name>
+    <message>
+        <location filename="../asw.cpp" line="121"/>
+        <source>Hello!</source>
+        <translation>Привет!</translation>
+    </message>
+    <message>
+        <location filename="../asw.cpp" line="146"/>
+        <source>Save history</source>
+        <translation>Сохранить историю сообщений</translation>
+    </message>
+    <message>
+        <location filename="../asw.cpp" line="146"/>
+        <location filename="../asw.cpp" line="161"/>
+        <source>JSON file</source>
+        <translation>Файл JSON</translation>
+    </message>
+    <message>
+        <location filename="../asw.cpp" line="156"/>
+        <source>Import message history?</source>
+        <translation>Импортировать историю сообщений?</translation>
+    </message>
+    <message>
+        <location filename="../asw.cpp" line="157"/>
+        <source>Messages will be added to the beginning of the dialogue.</source>
+        <translation>Сообщения будут добавлены в начало диалога.</translation>
+    </message>
+    <message>
+        <location filename="../asw.cpp" line="161"/>
+        <source>Load history</source>
+        <translation>Загрузить историю сообщений</translation>
+    </message>
+</context>
+<context>
+    <name>ASourceList</name>
+    <message>
+        <location filename="../widgets/a_sourcelist.cpp" line="8"/>
+        <source>Sources</source>
+        <translation>Источники</translation>
+    </message>
+</context>
+<context>
+    <name>Basis</name>
+    <message>
+        <location filename="../core/basis.cpp" line="10"/>
+        <source>An access error occurred (e.g. trying to write to a read-only file).</source>
+        <translation>Произошла ошибка доступа (например, попытка записи в файл только для чтения).</translation>
+    </message>
+    <message>
+        <location filename="../core/basis.cpp" line="14"/>
+        <source>A format error occurred (e.g. loading a malformed file).</source>
+        <translation>Произошла ошибка формата (например, загрузка поврежденного файла).</translation>
+    </message>
+</context>
+<context>
+    <name>CreateSourceDialog</name>
+    <message>
+        <location filename="../dialogues/sources/create-source.cpp" line="12"/>
+        <source>Save</source>
+        <translation>Сохранить</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/sources/create-source.cpp" line="13"/>
+        <source>Cancel</source>
+        <translation>Отменить</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/sources/create-source.cpp" line="16"/>
+        <source>Enter source name</source>
+        <translation>Введите название источника</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/sources/create-source.cpp" line="38"/>
+        <source>Select database file...</source>
+        <translation>Выберите файл базы данных...</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/sources/create-source.cpp" line="45"/>
+        <source>Select database...</source>
+        <translation>Выберите базу данных...</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/sources/create-source.cpp" line="45"/>
+        <source>ASW database</source>
+        <translation>База данных ASW</translation>
+    </message>
+</context>
+<context>
+    <name>EditSourceDialog</name>
+    <message>
+        <location filename="../dialogues/sources/edit-source.cpp" line="9"/>
+        <source>Activators</source>
+        <translation>Активаторы</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/sources/edit-source.cpp" line="10"/>
+        <source>Expressions</source>
+        <translation>Выражения</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/sources/edit-source.cpp" line="11"/>
+        <source>Reagents</source>
+        <translation>Реагенты</translation>
+    </message>
+</context>
+<context>
+    <name>QTranslator</name>
+    <message>
+        <location filename="../dialogues/about.cpp" line="30"/>
+        <location filename="../dialogues/about.cpp" line="42"/>
+        <location filename="../dialogues/firststart.cpp" line="26"/>
+        <source>Associative system</source>
+        <translation>Ассоциативная система</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/about.cpp" line="43"/>
+        <source>The ASW Authors</source>
+        <translation>Авторы ASW</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/about.cpp" line="49"/>
+        <source>License</source>
+        <translation>Лицензия</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/about.cpp" line="34"/>
+        <location filename="../dialogues/about.cpp" line="50"/>
+        <source>version</source>
+        <translation>версия</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/about.cpp" line="55"/>
+        <source>Shamshin Victor</source>
+        <translation>Шамшин Виктор</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/about.cpp" line="56"/>
+        <location filename="../dialogues/about.cpp" line="61"/>
+        <source>Developer</source>
+        <translation>Разработчик</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/about.cpp" line="60"/>
+        <source>Titov Klimenty</source>
+        <translation>Титов Климентий</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/about.cpp" line="74"/>
+        <location filename="../dialogues/firststart.cpp" line="41"/>
+        <source>Close</source>
+        <translation>Закрыть</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/about.cpp" line="87"/>
+        <source>About</source>
+        <translation>О приложении</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/about.cpp" line="88"/>
+        <source>Authors</source>
+        <translation>Авторы</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/sources.cpp" line="29"/>
+        <source>Add source</source>
+        <translation>Добавить источник</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/sources.cpp" line="30"/>
+        <source>Create</source>
+        <translation>Создать</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/sources.cpp" line="31"/>
+        <source>Remove</source>
+        <translation>Отключить</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/sources.cpp" line="32"/>
+        <source>Save and close</source>
+        <translation>Сохранить и закрыть</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/sources.cpp" line="60"/>
+        <source>Select database</source>
+        <translation>Выбрать базу данных</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/sources.cpp" line="61"/>
+        <source>ASW database</source>
+        <translation>База данных ASW</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/firststart.cpp" line="30"/>
+        <source>This is the first start of ASW. Before you start working:</source>
+        <translation>Это - первый запуск Ассиста. Прежде чем начать работать:</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/firststart.cpp" line="32"/>
+        <source>1) go to the Source Manager...</source>
+        <translation>1) перейдите в Менеджер источников...</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/firststart.cpp" line="34"/>
+        <source>2) add databases...</source>
+        <translation>2) добавьте базы данных...</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/firststart.cpp" line="36"/>
+        <source>And now you&apos;ve done.</source>
+        <translation>И теперь вы можете приступать к работе.</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/settings.cpp" line="15"/>
+        <source>Enable delay</source>
+        <translation>Включить задержку</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/settings.cpp" line="16"/>
+        <source>From</source>
+        <translation>От</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/settings.cpp" line="21"/>
+        <source>to</source>
+        <translation>до</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/settings.cpp" line="26"/>
+        <source>ms</source>
+        <translation>мс</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/settings.cpp" line="36"/>
+        <source>Keep history</source>
+        <translation>Хранить историю</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/settings.cpp" line="39"/>
+        <source>Enables ASW response delay.</source>
+        <translation>Включает задержку отправки ответа Ассиста.</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/settings.cpp" line="41"/>
+        <source>Enables keeping the message history after exiting the application.</source>
+        <translation>Позволяет сохранить историю сообщений после выхода из приложения.</translation>
+    </message>
+    <message>
+        <location filename="../dialogues/settings.cpp" line="44"/>
+        <source>OK</source>
+        <translation>ОК</translation>
+    </message>
+</context>
+<context>
+    <name>SQLite</name>
+    <message>
+        <location filename="../core/sqlite.cpp" line="306"/>
+        <location filename="../core/sqlite.cpp" line="325"/>
+        <source>Database</source>
+        <translation>База данных</translation>
+    </message>
+    <message>
+        <location filename="../core/sqlite.cpp" line="307"/>
+        <source>doesn&apos;t exist.</source>
+        <translation>не существует.</translation>
+    </message>
+    <message>
+        <location filename="../core/sqlite.cpp" line="326"/>
+        <source>is empty.</source>
+        <translation>пуста.</translation>
+    </message>
+</context>
+</TS>
