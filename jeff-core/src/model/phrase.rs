63bf1b83c6c7cc748717890b0f47fb49ef5fd264
Klimenty Titov <aclo.create@gmail.com>
2023-04-01 12:33:10 +0300

    Added more comments

--- jeff-core/src/model/phrase.rs
+++ jeff-core/src/model/phrase.rs
@@ -1,9 +1,9 @@
-//! TBD.
+//! Structs and functions needed for working with phrases routes.
 
 use serde::{Deserialize, Serialize};
 use serde_json::{Map, Value};
 
-/// TBD.
+/// Struct for creating phrase request (see crate::core_kit::rocket_phrases::create_phrase).
 #[derive(Deserialize, Serialize)]
 pub struct CreatePhraseRequest {
   pub phrase: String,
@@ -12,7 +12,7 @@ pub struct CreatePhraseRequest {
   pub properties: Map<String, Value>,
 }
 
-/// TBD.
+/// Struct for getting phrase request (see crate::core_kit::rocket_phrases::get_phrase).
 #[derive(Deserialize, Serialize)]
 pub struct GetPhraseResponse {
   pub id: i64,
@@ -22,7 +22,7 @@ pub struct GetPhraseResponse {
   pub properties: Map<String, Value>,
 }
 
-/// TBD.
+/// Links packer from `&Vec<i64>` to `String` (for db).
 pub fn pack_links(links: &Vec<i64>) -> String {
   let mut packed = String::new();
   for link in links {
@@ -33,7 +33,7 @@ pub fn pack_links(links: &Vec<i64>) -> String {
   packed
 }
 
-/// TBD.
+/// Links unpacker to `Vec<i64>` from `&String` (from db).
 pub fn unpack_links(links: &String) -> Vec<i64> {
   links.split(',').map(|el| el.parse::<i64>()).try_collect::<Vec<i64>>().unwrap_or(Vec::new())
 }
